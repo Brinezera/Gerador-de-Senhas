5744b1710d8ec66f1a0df6c7b83af38adcc26d5f
andrebrine11@gmail.com <andrebrine11@gmail.com>
2023-06-21 19:32:42 -0300

    função

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { nanoid } from "nanoid";
 
-const randomPassword = nanoid();
-console.log(randomPassword);
+const passwordBtnel = document.querySelector('button')
+const displayPasswordEl = document.querySelector('h2')
+
+passwordBtnel.addEventListener('click', () => {
+  const randomPassword = nanoid();
+  displayPasswordEl.innerHTML = randomPassword;
+});
